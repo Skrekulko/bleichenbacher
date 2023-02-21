3a5ccef018c6ad965c9ffb3e8b420ec37d89e1f1
Skrekulko <rybar.mtj@gmail.com>
2023-02-21 19:34:06 +0100

    feat: writable test results

--- tests/test_bleinchenbacher.py
+++ tests/test_bleinchenbacher.py
@@ -4,19 +4,43 @@ from bleichenbacher.pkcs import decode
 
 
 def test_bleichenbacher() -> None:
-    # RSA Oracle
-    oracle = Oracle(bits=256)
+    # RSA Sizes
+    #rsa_sizes = [256, 512, 1024, 2048, 4096]
+    rsa_sizes = [256]
 
-    # Message
-    message = b"RSA256"
+    # Check Difficulty (Simple, Advanced)
+    #check_types = [True, False]
+    check_types = [True]
 
-    # Ciphertext
-    ciphertext = oracle.encrypt(plaintext=message)
+    # Number Of Cycles
+    #n_cycles = 20
+    n_cycles = 5
 
-    # Run The Attack
-    data = bleichenbacher_chosen_plaintext(oracle=oracle, ciphertext=ciphertext, conforming=True)
+    # Test For Different RSA Sizes
+    for rsa_size in rsa_sizes:
+        # Test For Different Check Difficulty
+        for check_type in check_types:
+            # For 'n' Cycles
+            for _ in range(n_cycles):
+                # RSA Oracle
+                oracle = Oracle(bits=rsa_size, simple_check=check_type)
 
-    print(f"Recovered in: {data['time']}")
-    print(f"Calls to oracle: {data['calls']}")
+                # Message
+                message = b"RSA" + bytes(str(rsa_size), "ascii")
 
-    assert message == decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())
+                # Ciphertext
+                ciphertext = oracle.encrypt(plaintext=message)
+
+                # Run The Attack
+                data = bleichenbacher_chosen_plaintext(oracle=oracle, ciphertext=ciphertext, conforming=True)
+
+                with open("test_results.txt", "a", encoding="utf-8") as file:
+                    file.write(f"rsa_{rsa_size}:\n")
+                    file.write(f"\tsimple={check_type}\n")
+                    file.write(f"\ttime={data['time']}\n")
+                    file.write(f"\tcalls={data['calls']}\n")
+                    file.write(f"\toriginal={decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())}\n\n")
+
+                    file.close()
+
+                assert message == decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())
