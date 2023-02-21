9f6796df69770d983ee39f4e3620d99e6c57595e
Skrekulko <rybar.mtj@gmail.com>
2023-02-21 19:47:22 +0100

    refac: test file format

--- tests/test_bleinchenbacher.py
+++ tests/test_bleinchenbacher.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from bleichenbacher.oracle import Oracle
 from bleichenbacher.bleichenbacher import bleichenbacher_chosen_plaintext
 from bleichenbacher.pkcs import decode
@@ -14,14 +15,22 @@ def test_bleichenbacher() -> None:
 
     # Number Of Cycles
     #n_cycles = 20
-    n_cycles = 5
+    n_cycles = 1
+
+    # Create A New Test File
+    file = open("test_results.txt", "a", encoding="utf-8")
+
+    # Write The Date And Time Of The Test
+    file.write(f"{datetime.now()}\n")
 
     # Test For Different RSA Sizes
     for rsa_size in rsa_sizes:
         # Test For Different Check Difficulty
         for check_type in check_types:
             # For 'n' Cycles
-            for _ in range(n_cycles):
+            for n in range(n_cycles):
+                print(f"\nRSA-{rsa_size} | Simple PKCS Check ({check_type}) | Cycle {n + 1}")
+
                 # RSA Oracle
                 oracle = Oracle(bits=rsa_size, simple_check=check_type)
 
@@ -34,13 +43,14 @@ def test_bleichenbacher() -> None:
                 # Run The Attack
                 data = bleichenbacher_chosen_plaintext(oracle=oracle, ciphertext=ciphertext, conforming=True)
 
-                with open("test_results.txt", "a", encoding="utf-8") as file:
-                    file.write(f"rsa_{rsa_size}:\n")
-                    file.write(f"\tsimple={check_type}\n")
-                    file.write(f"\ttime={data['time']}\n")
-                    file.write(f"\tcalls={data['calls']}\n")
-                    file.write(f"\toriginal={decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())}\n\n")
-
-                    file.close()
+                # Write The Results To File
+                file.write(f"\trsa_{rsa_size}:\n")
+                file.write(f"\t\tsimple={check_type}\n")
+                file.write(f"\t\ttime={data['time']}\n")
+                file.write(f"\t\tcalls={data['calls']}\n")
+                file.write(f"\t\toriginal={decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())}\n\n")
 
                 assert message == decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())
+
+    # Close The File
+    file.close()
