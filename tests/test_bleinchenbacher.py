523c14578c092ebe5544e07fbd98b1bcb22f0d0b
Skrekulko <rybar.mtj@gmail.com>
2023-03-12 14:52:08 +0100

    refac: csv format

--- tests/test_bleinchenbacher.py
+++ tests/test_bleinchenbacher.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import csv
+from time import strftime
 from bleichenbacher.oracle import Oracle
 from bleichenbacher.bleichenbacher import bleichenbacher_chosen_plaintext
 from bleichenbacher.pkcs import decode
@@ -8,22 +9,31 @@ def test_bleichenbacher() -> None:
     # RSA Sizes
     rsa_sizes = [256, 512, 1024, 2048, 4096]
 
-    # Check Difficulty (Simple, Advanced)
-    check_types = [True, False]
+    # PKCS Conformity Check (Simple, Advanced)
+    conformity_checks = [True, False]
 
     # Number Of Cycles
     n_cycles = 20
 
+    # Create A New CSV Test File
+    file = open(f"tests/test_{strftime('%Y_%m_%d-%H_%M_%S')}.csv", "w", encoding="utf-8")
+
+    # Create A CSV Handle
+    writer = csv.writer(file)
+
+    # Add CSV File Header
+    writer.writerow(["rsa_size", "simple", "time", "calls", "recovered"])
+
     # Test For Different RSA Sizes
     for rsa_size in rsa_sizes:
         # Test For Different Check Difficulty
-        for check_type in check_types:
+        for conformity_check in conformity_checks:
             # For 'n' Cycles
             for n in range(n_cycles):
-                print(f"\nRSA-{rsa_size} | Simple PKCS Check ({check_type}) | Cycle {n + 1}")
+                print(f"\nRSA-{rsa_size} | Simple PKCS Check ({conformity_check}) | Cycle {n + 1}")
 
                 # RSA Oracle
-                oracle = Oracle(bits=rsa_size, simple_check=check_type)
+                oracle = Oracle(bits=rsa_size, simple_check=conformity_check)
 
                 # Message
                 message = b"RSA" + bytes(str(rsa_size), "ascii")
@@ -36,19 +46,8 @@ def test_bleichenbacher() -> None:
 
                 assert message == decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())
 
-                # Create A New Test File
-                file = open("test_results.txt", "a", encoding="utf-8")
+                # Write The Results To The CSV File
+                writer.writerow([rsa_size, conformity_check, data['time'], data['calls'], data['recovered_message']])
 
-                # Write The Results To File
-                file.write(f"{datetime.now()}\n")
-                file.write(f"\trsa_{rsa_size}:\n")
-                file.write(f"\t\tsimple={check_type}\n")
-                file.write(f"\t\ttime={data['time']}\n")
-                file.write(f"\t\tcalls={data['calls']}\n")
-                file.write(
-                    f"\t\trecovered="
-                    f"{decode(message=data['recovered_message'], n_size=oracle.parameters.size_in_bytes())}\n\n"
-                )
-
-                # Close The File
-                file.close()
+    # Close The File
+    file.close()
